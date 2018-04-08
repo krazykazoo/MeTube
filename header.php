8248ed9efc57dc21d3bb0ac3bb07650280c141f9
karl <karljamesp@gmail.com>
2018-04-07 23:40:27 -0400

    header.php

--- header.php
+++ header.php
@@ -4,13 +4,8 @@ ini_set('session.save_path', '/home/kpascia/sessions');
 ini_set('session.gc_probability', 1);
 session_start();
 
-function logout() {
-	session_unset(); 
-	header("Location: index.php");
-}
-
 if (isset($_SESSION['username'])) {
-	echo '<form method = "post" action = "<?php logout(); ?>"> <input type="submit" value="Logout"> </form>';
+	echo '<form method = "post" action = "logout.php"> <input type="submit" value="Logout"> </form>';
 }
 
 
