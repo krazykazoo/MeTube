843b913cf8e2b772b9203954a475947b2425975c
karl <karljamesp@gmail.com>
2018-04-07 22:42:26 -0400

    browse.php

--- media.php
+++ media.php
@@ -34,7 +34,7 @@ if(isset($_GET['id'])) {
 	else //view movie
 	{	
 ?>
-	<!-- <p>Viewing Video:<?php echo $result_row[2].$result_row[];?></p> -->
+	<!-- <p>Viewing Video:<?php echo $result_row['type'].$result_row['name'];?></p> -->
 	<p>Viewing Video:<?php echo $result_row['name'];?></p>
 	      
     <object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
