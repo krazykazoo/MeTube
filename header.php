5721c7b67cd4e19b5ad0d74afd163f65418b266c
karl <karljamesp@gmail.com>
2018-04-07 23:21:06 -0400

    header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@ ini_set('session.gc_probability', 1);
 session_start();
 
 if (isset($_SESSION['username'])) {
-	echo '<input type="button" action="<?php session_destroy();?>" value="Logout">'
+	echo '<input type="button" action="<?php session_destroy();?>" value="Logout">';
 }
 ?>
 
