2dce266b8c6527576a2446caa64ca083225217a2
karl <karljamesp@gmail.com>
2018-04-07 23:30:55 -0400

    header.php

--- header.php
+++ header.php
@@ -4,14 +4,16 @@ ini_set('session.save_path', '/home/kpascia/sessions');
 ini_set('session.gc_probability', 1);
 session_start();
 
-if (isset($_SESSION['username'])) {
-	echo '<input type="button" action="<?php logout(); ?>" value="Logout">';
-}
-
-
 function logout() {
 	session_unset(); 
 	header("Location: index.php");
 }
+
+if (isset($_SESSION['username'])) {
+	echo '<button type="button" action="<?php logout(); ?>" value="Logout">';
+}
+
+
+
 
 ?>
