b1cff8cb84c605a49e36c3d2a857c1843f493ea8
karl <karljamesp@gmail.com>
2018-04-09 11:17:29 -0400

    upload_process.php

---
+++ search.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html> 
+
+	<?php
+		include_once 'header.php';
+	?>	
+	
+	<div id='upload_result'>
+		<?php 
+			if(isset($_REQUEST['result']) && $_REQUEST['result']!=0) {		
+				echo upload_error($_REQUEST['result']);
+			}
+		?>
+	</div>
+	<br/><br/>
+	<?php
+		$query = "SELECT * from Media"; 
+		$result = mysql_query( $query );
+		if (!$result) {
+		   die ("Could not query the media table in the database: <br />". mysql_error());
+		}
+	?>
+	    
+    <div style="background:#339900;color:#FFFFFF; width:150px;">Uploaded Media</div>
+	<table width="50%" cellpadding="0" cellspacing="0">
+		<?php
+			while ($result_row = mysql_fetch_assoc($result)) //filename, username, type, mediaid, path
+			{ 
+				$mediaid = $result_row['media_id'];
+				$filename = $result_row['name'];
+				$filenpath = $result_row['path'];
+				$user = $result_row['username']
+		?>
+        <tr valign="top">			
+			<td>
+				<?php 
+					echo $user;  //mediaid
+				?>
+			</td>
+            <td>
+            	<a href="media.php?id=<?php echo $mediaid;?>" target="_blank"><?php echo $filename;?></a> 
+           	</td>
+           	<td>
+           		<a href="<?php echo $filenpath;?>" target="_blank" onclick="javascript:saveDownload(<?php echo $result_row[4];?>);">Download</a>
+			</td>
+		</tr>
+        <?php
+			}
+		?>
+	</table>
+	
+
+
+</html>
