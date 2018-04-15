aae73f7eba26947938505875236f5c2fe800c82b
karl <karljamesp@gmail.com>
2018-04-15 19:12:43 -0400

    comment work

--- comment.php
+++ comment.php
@@ -4,7 +4,7 @@
 		$content = $_POST['message'];
 		$media_id = $_POST['media_id'];
 		$username = $_SESSION['username'];
-		$query = "INSERT INTO Comment (media_fk, username, content) VALUES ('$media_id', '$username', '$content')"
+		$query = "INSERT INTO Comment (media_fk, username, content) VALUES ('$media_id', '$username', '$content')";
 		$result = mysql_query($query);
 		if ($result) {
 			header("Location: media.php?id=$media_id");	
