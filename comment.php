78f3ffe77caa6f5123e03fea05252bc0e28122d3
karl <karljamesp@gmail.com>
2018-04-15 19:12:04 -0400

    comment work

--- comment.php
+++ comment.php
@@ -2,7 +2,7 @@
 	if (isset($_POST['message']) &&
 		isset($_POST['media_id'])) {
 		$content = $_POST['message'];
-		$media_id = $_POST['media_id']
+		$media_id = $_POST['media_id'];
 		$username = $_SESSION['username'];
 		$query = "INSERT INTO Comment (media_fk, username, content) VALUES ('$media_id', '$username', '$content')"
 		$result = mysql_query($query);
