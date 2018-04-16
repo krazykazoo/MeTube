77ff0a0b78e518c10e5b9d9c8ba2ac0899c0e0a4
karl <karljamesp@gmail.com>
2018-04-16 19:58:27 -0400

    playlist work

--- contacts.php
+++ contacts.php
@@ -21,7 +21,7 @@
 				$userResult = mysql_query($getUserId);
 				$row = mysql_fetch_assoc($userResult);
 				$username = $row['username'];
-				echo "<tr><td>$username</td><td><a href='channel.php?id=$username'>Link</a></td></tr>";
+				echo "<tr><td>$username</td><td><a href='channel.php?username=$username'>Link</a></td></tr>";
 			}
 		}
 		
