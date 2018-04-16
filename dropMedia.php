5655007b485db6eeb55e0fe1bd6ea118411605c1
karl <karljamesp@gmail.com>
2018-04-16 19:22:59 -0400

    playlist work

--- dropMedia.php
+++ dropMedia.php
@@ -3,8 +3,8 @@
 	include_once 'function.php';
 	if (isset($_GET['playlist_name']) &&
 		isset($_GET['id'])) {
-		$name = $_POST['playlist_name'];
-		$media_id = $_POST['id'];
+		$name = $_GET['playlist_name'];
+		$media_id = $_GET['id'];
 		$getUserId = "SELECT * FROM Account WHERE username = '".$_SESSION['username']."'";
 		$userResult = mysql_query($getUserId);
 		$row = mysql_fetch_assoc($userResult);
