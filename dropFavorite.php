4a32ec3e9f2a0f6c872794c44eeb775adaa2f4c5
karl <karljamesp@gmail.com>
2018-04-17 11:42:56 -0400

    favorites work

--- dropFavorite.php
+++ dropFavorite.php
@@ -2,7 +2,7 @@
 	include_once 'header.php';
 	include_once 'function.php';
 	if (isset($_POST['media_id'])) {
-		$media_id = $_POST['user'];
+		$media_id = $_POST['media_id'];
 		$getUserId = "SELECT * FROM Account WHERE username = '".$_SESSION['username']."'";
 		$userResult = mysql_query($getUserId);
 		$row = mysql_fetch_assoc($userResult);
