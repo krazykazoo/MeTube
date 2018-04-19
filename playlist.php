8d45ae9f8a9beb9473b05576f64126b7d6c5f844
bradgnewman <bradgnewman@gmail.com>
2018-04-18 22:44:50 -0400

    Add files via upload

--- playlist.php
+++ playlist.php
@@ -1,8 +1,29 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<head>
+<style type="text/css">
+.buttonz {
+    background-color: #F66733; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    cursor: pointer;
+    width: 150px;
+		margin: 1px;
+}
+
+.buttonz:hover {
+    background-color: #522D80;
+}
+</style>
+</head>
 <?php
 	include_once 'header.php';
 	include_once "function.php";
-	
+
 	if (!isset($_GET['id'])) {
 		$getUserId = "SELECT * FROM Account WHERE username = '".$_SESSION['username']."'";
 		$userResult = mysql_query($getUserId);
@@ -31,15 +52,17 @@
 	if (mysql_num_rows($result) > 0) {
 	$query = "UPDATE Media SET views = views + 1 WHERE media_id = '$media_id'";
 	mysql_query($query);
-	
+
 	$query = "SELECT * FROM Media WHERE media_id = '$media_id'";
 	$result = mysql_query( $query );
 	$result_row = mysql_fetch_assoc($result);
-		
+
 	$filename=$result_row['name'];   ////0, 4, 2
-	$filepath=$result_row['path']; 
+	$filepath=$result_row['path'];
 	$type=$result_row['type'];
 	$media_id = $result_row['media_id'];
+	$views = $result_row['views'];
+	$uploader = $result_row['username'];
 	if(substr($type,0,5)=="image") //view image
 	{
 		echo "Viewing Picture: ";
@@ -56,30 +79,38 @@
   <source src="<?php echo $filepath?>" type="video/mp4">
   <source src="<?php echo $filepath?>" type="video/ogg">
 Your browser does not support the video tag.
-</video> 
+</video>
+<br />
+<span><em>Views: <?php echo $views; ?> </em></span>
+<span>&nbsp;&nbsp;&nbsp;&nbsp; Uploaded By: <?php echo $uploader; ?></span>
 <br />
 
 
 <?php } ?>
- 
+
  <?php if ($next_media != 0) { ?>
-<a href="playlist.php?id=<?php echo $next_media;?>">Next</a>
-<?php 
+<a href="playlist.php?id=<?php echo $next_media;?>"><button class="buttonz" type="button">Next</button></a>
+<?php
 	}
 	else {?>
-<a href="playlist.php">Beginning</a>
+<br />
+<span><em>Views: <?php echo $views; ?> </em></span>
+<span>&nbsp;&nbsp;&nbsp;&nbsp; Uploaded By: <?php echo $uploader; ?></span>
+<br />
+<a href="playlist.php"><button class="buttonz" type="button">Beginning</button></a>
 <?php } ?>
 	<table style="width:100%">
 		<tr>
 			<td style="width:20%"> User </td>
 			<td style="width:80%"> Comment </td>
 		</tr>
-	<a href="dropMedia.php?id=<?php echo $media_id; ?>&playlist_name=<?php echo $playlist_name; ?>">Remove</a>
-		          
+		<a href="dropMedia.php?id=<?php echo $media_id; ?>&playlist_name=<?php echo $playlist_name; ?>"><button class="buttonz" type="button">Remove</button></a>
+		<a href="<?php echo $filepath;?>" target="_blank" onclick="javascript:saveDownload(<?php echo $result_row[4];?>);"><button class="buttonz" type="button">Download</button></a>
+
 <?php
 	$getComments = "SELECT * FROM Comment WHERE media_fk = '$media_id'";
 	$commentResult = mysql_query($getComments);
-	
+
 	while ($row = mysql_fetch_assoc($commentResult)) {
 		$user = $row['username'];
 		$content = $row['content'];
@@ -96,7 +127,7 @@ Your browser does not support the video tag.
 			<form method="post" action="comment.php">
 				<input type="hidden" name="media_id" value="<?php echo $media_id;?>">
 				<span>Message: </span><input type="text" name="message" required>
-				<input type="submit" value="Send"></button>
+				<input type="submit" value="Post Comment"></button>
 			</form>
 		</td>
 	</tr>
